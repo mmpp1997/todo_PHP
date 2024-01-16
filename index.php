c55e489f64a49904d6a004fa9c477c9b52b9b164
Mario Pedisic <mario.pedisic@gmail.com>
2024-01-16 11:58:59 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+define("NAME", "Mario");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My page</title>
+</head>
+
+<body>
+    <h1><?php echo NAME; ?></h1>
+
+</body>
+
+</html>
